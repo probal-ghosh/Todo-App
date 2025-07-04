19f20f70813e6b24d3c46a7683364ce2d6395a5c
Probal Ghosh <probalghoshofficial@gmail.com>
2025-07-04 20:33:07 +0530

    added context & context provider

--- src/component/TodoForm.jsx
+++ src/component/TodoForm.jsx
@@ -1,4 +1,13 @@
+import { useState } from "react";
+
+
+
 function TodoForm() {
+    const [Todo, setTodo ] = useState([])
+    setTimeout(()=>{
+        console.log(Todo)
+    },2000)
+    
     
 
     return (
@@ -7,6 +16,8 @@ function TodoForm() {
                 type="text"
                 placeholder="Write Todo..."
                 className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+                value={Todo}
+                onChange={(e)=>{setTodo(e.target.value)}}
             />
             <button type="submit" className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0">
                 Add
